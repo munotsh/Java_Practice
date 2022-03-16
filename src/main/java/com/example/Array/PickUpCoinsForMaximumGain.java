db7ce408b0321d31924d4072978aebdb06d1b606
Lenovo <shaileshmunot99@gmail.com>
2022-03-16 16:03:47 +0530

    PickUpCoinsForMaximumGain

---
+++ src/main/java/com/example/Array/PickUpCoinsForMaximumGain.java
@@ -0,0 +1,37 @@
+package com.example.Array;
+
+import java.util.Arrays;
+
+public class PickUpCoinsForMaximumGain {
+    public static void main(String[] args) {
+
+        int[] arr = {5, 25, 10, 1};
+        System.out.println(pickUpCoins(arr));
+    }
+
+
+    public static int pickUpCoins(int[] C) {
+        int[][] T = new int[C.length][C.length];
+        for (int[] t : T) {
+            Arrays.fill(t, -1);
+        }
+        return pickUpCoinsHelper(C, 0, C.length - 1, T);
+    }
+
+    private static int pickUpCoinsHelper(int[] C, int a, int b,int[][] T) {
+        if (a > b) {
+            return 0; // Base condition.
+        }
+
+        if (T[a][b] == -1) {
+            T[a][b] = Math.max(
+                    C[a]+ Math.min(pickUpCoinsHelper(C, a + 2, b, T),
+                            pickUpCoinsHelper(C, a + 1, b - 1, T)),
+                    C[b]
+                            + Math.min(pickUpCoinsHelper(C, a + 1, b - 1, T),
+                            pickUpCoinsHelper(C, a, b - 2, T))
+            );
+        }
+        return T[a][b];
+    }
+}

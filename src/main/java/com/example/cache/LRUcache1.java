6c9c3681f48d3d8f8cc9c0649189e1806a985f2b
prita <shaileshmunot1991@gmail.com>
2024-01-24 15:41:02 -0500

    cache problem

--- src/main/java/com/example/cache/LRUcache1.java
+++ src/main/java/com/example/cache/LRUcache1.java
@@ -1,101 +1,64 @@
 package com.example.cache;
 
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class LRUcache1 {
-	class Node {
-	    public int key;
-	    public int val;
-	    public Node next;
-	    public Node prev;
 
-	    public Node(int key, int val) {
-	        this.key = key;
-	        this.val = val;
-	        next = null;
-	        prev = null;
-	    }
-	}
-	 private Map<Integer, Node> cache;
-	    private Node head;
-	    private Node tail;
-	    private int capacity;
+    int capacity;
+    Map<Integer, Integer> map = new LinkedHashMap<>();
 
-	    public LRUcache1(int capacity) {
-	        this.capacity = capacity;
-	        cache = new HashMap<>();
-	        head = new Node(-1, -1);
-	        tail = new Node(-1, -1);
-	        head.next = tail;
-	        tail.prev = head;
-	    }
+    public LRUcache1(int capacity) {
+        this.capacity = capacity;
+    }
 
-	    private void deleteNode(Node node) {
-	        Node prev = node.prev;
-	        Node next = node.next;
-	        prev.next = next;
-	        next.prev = prev;
-	    }
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            Integer val = map.get(key);
+            map.remove(key);
 
-	    private void addNode(Node newNode) {
-	        Node temp = head.next;
-	        head.next = newNode;
-	        newNode.prev = head;
-	        newNode.next = temp;
-	        temp.prev = newNode;
-	    }
+            map.put(key, val);
+            return map.get(key);
+        } else return -1;
+    }
 
-	    public int get(int key) {
-	        if (!cache.containsKey(key))
-	            return -1;
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            map.remove(key);
+            map.put(key, value);
+        } else {
+            if (map.size() < capacity) {
+                map.put(key, value);
+            } else {
+                // removing first element in the map.
+                Iterator<Integer> iterator = map.keySet().iterator();
+                map.remove(iterator.next());
+                map.put(key, value);
+            }
+        }
+    }
 
-	        Node node = cache.get(key);
-	        deleteNode(node);
-	        addNode(node);
-	        cache.put(key, head.next);
-	        return head.next.val;
-	    }
+    public void print() {
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+            System.out.println(e.getValue());
+        }
+    }
 
-	    public void put(int key, int value) {
-	        if (cache.containsKey(key)) {
-	            Node node = cache.get(key);
-	            deleteNode(node);
-	            node.val = value;
-	            addNode(node);
-	            cache.put(key, head.next);
-	        } else {
-	            if (cache.size() == capacity) {
-	                Node prev = tail.prev;
-	                deleteNode(prev);
-	                cache.remove(prev.key);
-	                Node newNode = new Node(key, value);
-	                addNode(newNode);
-	                cache.put(key, head.next);
-	            } else {
-	                Node newNode = new Node(key, value);
-	                addNode(newNode);
-	                cache.put(key, head.next);
-	            }
-	        }
-	    }
-	    
-	    public static void main(String[] args) {
-	    	LRUcache1 lrUcache = null;
-	    	String[] strArr = {"LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"};
-	    	int[][] value = {{1},{1,1},{2,2},{1},{3,3},{2},{4,4},{2},{3},{4}};
-	    	for(int i=0;i<strArr.length;i++) {
-	    		if(strArr[i].equals("LRUCache")) {
-	    			lrUcache = new LRUcache1(value[i][0]);
-	    		}
-	    		else if(strArr[i].equals("get")) {
-	    			lrUcache.get(value[i][0]);
-	    		}
-	    		else {
-	    			lrUcache.put(value[i][0], value[i][1]);
-	    		}
-	    	}
-	    	System.out.println(value);
-	    }
+    public static void main(String[] args) {
+        LRUcache1 lrUcache = null;
+        String[] strArr = {"LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"};
+        int[][] value = {{1}, {1, 1}, {2, 2}, {1}, {3, 3}, {2}, {4, 4}, {2}, {3}, {4}};
+        for (int i = 0; i < strArr.length; i++) {
+            if (strArr[i].equals("LRUCache")) {
+                lrUcache = new LRUcache1(value[i][0]);
+            } else if (strArr[i].equals("get")) {
+                lrUcache.get(value[i][0]);
+            } else {
+                lrUcache.put(value[i][0], value[i][1]);
+            }
+        }
+        lrUcache.print();
+    }
 
 }
